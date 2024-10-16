110720179507f4cf6426b6f5187eaaab4eccf25b
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2024-10-16 19:52:32 +0200

    initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,32 @@
+import styles from "./Header.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+import logo from "../../sprites/logo.svg"
+
+
+export default function Header() {
+    return (
+        <>
+            <div className={`${styles.headerContainer}`}>
+                <div className={styles.logoContainer}>
+                    <img src={logo} className={`${styles.logo} img-fluid`} alt="logo" />
+                </div>
+                <div className="pt-4">
+                    <h1 className={`${styles.logoContainer}`}>
+                        <FontAwesomeIcon icon={faCalendar} className={`${styles.calendar}`} />
+                        9 DE MAYO
+                    </h1>
+                    <h1 className={`${styles.logoContainer}`}>
+                        VUELVE CON MÁS FUERZA QUE NUNCA EL GRAN EVENTO TECNOLÓGICO CORDOBÉS
+                    </h1>
+                </div>
+                <div className="pt-4">
+                    <p className={`${styles.logoContainer}`}>
+                    <button>Call4Paper</button>
+                    </p>
+                </div>
+
+            </div>
+        </>
+    );
+}
