42e9bc2ccd069602aceeaffc6360aa911125ace3
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2024-10-22 11:40:35 +0200

    fix: logo in footer now responsive

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,18 @@
 import styles from "./Footer.module.css";
-import logo from "../../sprites/logoAula.png"; // Importing the logo image
+import logo from "../../sprites/logoAula.png";
 
 export default function Footer() {
   return (
     <footer className={styles.footerContainer}>
-      <div className={styles.footerContent}>
-        <div className={styles.title}>
-          Organiza:
-        </div>
-        <div className="row">
-          <div className="col">
-            <img src={logo} className={styles.logo} alt="Aula Software Libre" />
-          </div>
-          <div className="col">
-
+      <div className="row container">
+        <div className="col-md-6 col-sm-12 mt-5">
+          <div className="row py- justify-content-center">
+            <div className="col-4">
+              <div className={`${styles.title}`}>
+                Organiza:
+              </div>
+              <img src={logo} className={`img-fluid ${styles.logo}`} alt="Logo" />
+            </div>
           </div>
         </div>
       </div>
