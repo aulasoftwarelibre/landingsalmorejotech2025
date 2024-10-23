75511757dbcd1df41a75b762b95e085093215346
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2024-10-24 01:27:43 +0200

    feat: parralax effect added in footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import styles from "./Footer.module.css";
 import logo from "../../sprites/logoAula.png";
 import twitter from '../../sprites/featherIcons/twitter.svg'
@@ -10,11 +11,34 @@ import linkedln from '../../sprites/featherIcons/linkedin.svg'
 import mail from '../../sprites/featherIcons/mail.svg'
 import map from '../../sprites/featherIcons/map.svg'
 
+
 export default function Footer() {
+
+  const [isMoved, setIsMoved] = useState(false);
+
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+
+    // Trigger the movement when the user scrolls down a certain amount (e.g., 200px)
+    if (scrollPosition > 200) {
+      setIsMoved(true);
+    } else {
+      setIsMoved(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return  (
     <div className={styles.fondo1} id="footer">
       <div className="row">
-        <div className="col-md-6 col-sm-12 pb-md-5 pb-sm-3">
+      <div className={`col-md-6 col-sm-12 pb-md-5 pb-sm-3 ${styles.movableDivLeft} ${isMoved ? styles.moved : ''}`}>
           <div className="row pt-2">
             <h2>Organiza:</h2>
           </div>
@@ -22,11 +46,9 @@ export default function Footer() {
             <div className="col-12">
               <img src={logo} className="img-fluid col-4 " />
             </div>
-
           </div>
           <div className="row">
             <div className="col-12">
-
               <a href="https://www.facebook.com/AulaSoftwareLibre/" target="_blank" style={{ padding: "2%" }}>
                 <img src={facebook} alt="Facebook" style={{ width: "2em", height: "2em" }} />
               </a>
@@ -51,7 +73,7 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <div className="col-md-6 col-sm-12 py-5 d-flex flex-column align-items-center justify-content-center">
+        <div className={`col-md-6 col-sm-12 py-5 d-flex flex-column align-items-center justify-content-center ${styles.movableDivRight} ${isMoved ? styles.movedLeft : ''}`}>
           <div className="row">
             <div className="col-11">
               <h3>¡Encuéntranos!</h3>
