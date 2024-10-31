898c31693ab0d56efde2e7c7df6a0ef841b788b9
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2024-10-31 15:47:14 +0100

    feat: added some input fields in call4paper

--- src/pages/call4paper/call4paper.tsx
+++ src/pages/call4paper/call4paper.tsx
@@ -16,9 +16,22 @@ export default function Call4Paper() {
             <p className={`${styles.formText}`}>SOBRE LA ACTIVIDAD...</p>
             <input type="text" name="title" placeholder="Título" className={`${styles.inputForm}`}/>
             <select name="activityType" className={`${styles.inputForm}`}>
-                <option value="tenis">Charla ~ 35 min</option>
-                <option value="futbol">Keynote ~ 1 hora</option>
+                <option value="charla">Charla ~ 35 min</option>
+                <option value="keynote">Keynote ~ 1 hora</option>
             </select>
+            <select name="difficulty" className={`${styles.inputForm}`}>
+                <option value="baja">Baja</option>
+                <option value="media">Media</option>
+                <option value="alta">Alta</option>
+            </select>
+            <textarea name="descripcion" placeholder="Descripción de la Actividad" className={`${styles.inputFormArea}`}>
+            </textarea>
+            <select name="difficulty" className={`${styles.inputForm}`}>
+                <option value="baja">Baja</option>
+                <option value="media">Media</option>
+                <option value="alta">Alta</option>
+            </select>
+            
         </div>
     </>
 }
