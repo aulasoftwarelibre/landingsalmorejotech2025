2398e934822aa97c2b4737525884334245b54e22
darkghost078 <daviddarkghost@gmail.com>
2024-11-06 15:17:51 +0100

    feat: Parallax effect added in almost all the screens(excluding ipads and tablets)

--- src/supabase/server.tsx
+++ src/supabase/server.tsx
@@ -1,3 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(import.meta.env.VITE_SOME_KEY|| " ", import.meta.env.VITE_SUPABASE_ANON_KEY ||" ");
+export const supabase = createClient("https://gtvwlxhwpngxrfmrqzar.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0dndseGh3cG5neHJmbXJxemFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcyODEyMzYsImV4cCI6MjA0Mjg1NzIzNn0.4n7preawGleulQXLrMiAAQ5EK1qh4tI1C--Jy8gAG9c")
