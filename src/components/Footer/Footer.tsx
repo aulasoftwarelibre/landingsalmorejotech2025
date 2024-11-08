7e51dc6188182da06f73976fc1bdf60d486e6a75
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2024-11-08 11:01:12 +0100

    feat: twitter icon changed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -107,11 +107,16 @@ export default function Footer() {
                 target="_blank"
                 style={{ padding: '2%' }}
               >
-                <img
-                  src={twitter}
-                  alt="Twitter"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
                   style={{ width: '2em', height: '2em' }}
-                />
+                >
+                  <path
+                    fill="#ffffff"
+                    d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+                  />
+                </svg>
               </a>
               <a
                 href="https://github.com/aulasoftwarelibre"
